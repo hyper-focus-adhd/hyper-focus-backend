af74a92119853aa464ebff66dfb42517f413f343
david-sds <dave.sathler@gmail.com>
2023-04-23 22:14:43 -0300

    feat(user-auth): cors configuration

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import * as cors from 'cors';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    cors({
+      origin: process.env.FRONTEND_URL,
+    }),
+  );
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
