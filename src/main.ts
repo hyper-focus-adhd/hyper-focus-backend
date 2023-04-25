946dd6617fbcbc1d778a557d8eaf726bf3f784d7
Pedro Henrique Moreira <pedrohmoreira87@gmail.com>
2023-04-25 00:09:57 -0300

    feat(note): add notes crud

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,11 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import * as cors from 'cors';
 
 import { AppModule } from './app.module';
 import { corsConfig } from './config/cors.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(
-    cors({
-      origin: process.env.FRONTEND_URL,
-    }),
-  );
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
