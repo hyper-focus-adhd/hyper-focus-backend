ad9fc39020b31ef8c2bc46975df5223f8722d993
Pedro Henrique Moreira <pedro.moreira@aoatechpartners.com.br>
2023-11-07 20:58:25 -0300

    fix(bucket-key): add a bucket key to the env

--- src/common/helpers/storage-data-helper.ts
+++ src/common/helpers/storage-data-helper.ts
@@ -1,9 +1,10 @@
+import 'dotenv/config';
 import { Storage } from '@google-cloud/storage';
 
 export const storageDataHelper = {
   provide: Storage,
   useValue: new Storage({
     projectId: 'hyper-focus-389222',
-    keyFilename: 'keys/hyper-focus-389222-ceb716a7a3ef.json',
+    keyFilename: process.env.BUCKET_KEY,
   }),
 };
