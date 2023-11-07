4809cd5ea5d3fc3665a01b3e235b88046609ad76
Pedro Henrique Moreira <pedro.moreira@aoatechpartners.com.br>
2023-11-07 20:32:31 -0300

    fix(port-env): fix port env

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import * as process from 'process';
+
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
@@ -29,7 +31,7 @@ async function bootstrap(): Promise<void> {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 8080);
 }
 
 bootstrap();
