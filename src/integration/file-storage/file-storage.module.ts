3a3d3971a1292bf9ab22469af31840d9b8b7209f
Pedro Henrique Moreira <pedro.moreira@aoatechpartners.com.br>
2023-11-07 23:06:23 -0300

    fix(google-storage): fix google storage problem

--- src/integration/file-storage/file-storage.module.ts
+++ src/integration/file-storage/file-storage.module.ts
@@ -1,10 +1,9 @@
-import { Storage } from '@google-cloud/storage';
 import { Module } from '@nestjs/common';
 
 import { FileStorageService } from './file-storage.service';
 
 @Module({
   providers: [FileStorageService],
-  exports: [Storage, FileStorageService],
+  exports: [FileStorageService],
 })
 export class FileStorageModule {}
