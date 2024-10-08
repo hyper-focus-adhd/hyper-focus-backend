139de6c32a64a97935965a720783177f6db4a856
PedroHenriqueMoreira <pedrohmoreira87@gmail.com>
2024-10-08 17:09:10 -0400

    fix(eslint-yarn): added eslint and updated yarn

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,5 @@
 import pluginJs from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
 import perfectionist from 'eslint-plugin-perfectionist';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
@@ -47,4 +48,5 @@ export default [
       },
     },
   },
+  eslintConfigPrettier,
 ];
